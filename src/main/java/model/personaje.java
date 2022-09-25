bd3baec83ef5a55435c31352bdfb8c2ff58f1f4f
Christian Herrera <christianherrera@live.com.mx>
2022-09-24 22:09:19 -0500

    Personaje model package & class added

---
+++ src/main/java/model/personaje.java
@@ -0,0 +1,24 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class personaje {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idPersonaje;
+	private String imagen;
+	private String nombre;
+	private int edad;
+	private double peso;
+	private String historia;
+	
+	
+}
