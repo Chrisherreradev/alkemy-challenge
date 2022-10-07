352c0528eca7b49b80657e00c81b6b16f770a55b
Christian Herrera <christianherrera@live.com.mx>
2022-10-06 21:16:13 -0500

    PeliculaSerieRepository custom query methods added, PeliculaSerieService class created

--- src/main/java/repository/PeliculaSerieRepository.java
+++ src/main/java/repository/PeliculaSerieRepository.java
@@ -1,11 +1,26 @@
 package repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import model.PeliculaSerie;
 
 @Repository
 public interface PeliculaSerieRepository extends JpaRepository<PeliculaSerie, Long>{
+
+	public List<PeliculaSerie> findByTitle(String titulo);
+
+	@Query(value="SELECT imagen, titulo, fechaCreacion FROM Peli_Serie ORDER BY fechaCreacion Asc")
+	public List<PeliculaSerie> getAllByOrderAsc();
+
+	@Query(value="SELECT imagen, titulo, fechaCreacion FROM Peli_Serie ORDER BY fechaCreacion Desc")
+	public List<PeliculaSerie> getAllByOrderDesc();
+	public PeliculaSerie getById(Long idPelicula);
+
+	@Query(value="SELECT imagen, titulo, fechaCreacion FROM Peli_Serie")
+	public List<PeliculaSerie> getAll();
 
 }
---
+++ src/main/java/service/PeliculaSerieService.java
@@ -0,0 +1,48 @@
+package service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import model.PeliculaSerie;
+import repository.PeliculaSerieRepository;
+
+@Service
+public class PeliculaSerieService {
+
+@Autowired
+private PeliculaSerieRepository peliculaSerieRepository;
+
+public List<PeliculaSerie>findAll(Sort sort){
+	return peliculaSerieRepository.findAll();
+}
+public Optional<PeliculaSerie>findById(Long idPelicula){
+	return peliculaSerieRepository.findById(idPelicula);
+}
+public List<PeliculaSerie>findByTitle(String titulo){
+	return peliculaSerieRepository.findByTitle(titulo);
+}
+public PeliculaSerie save(PeliculaSerie peliculaSerie) {
+	return peliculaSerieRepository.save(peliculaSerie);
+}
+public List<PeliculaSerie>getByOrder(String orden){
+	if(orden.equals("ASC")) {
+		return peliculaSerieRepository.getAllByOrderAsc();
+	}else if(orden.equals("DES")) {
+		return peliculaSerieRepository.getAllByOrderDesc();
+	}else {
+		return peliculaSerieRepository.getAll();
+	}
+}
+public boolean delete(Long idPelicula) {
+	try {
+		peliculaSerieRepository.deleteById(idPelicula);
+		return true;
+	}catch(Exception e) {
+		return false;
+	}
+}
+}
