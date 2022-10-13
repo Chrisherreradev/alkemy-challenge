aacf6c29d9bf1b6b33ade7289e6727fe0454b035
Christian Herrera <christianherrera@live.com.mx>
2022-10-13 18:57:23 -0500

    GetMapping  by age method added, Delete, save methods added

--- src/main/java/controller/PersonajeController.java
+++ src/main/java/controller/PersonajeController.java
@@ -1,15 +1,23 @@
 package controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import model.Personaje;
 import service.PersonajeService;
@@ -32,5 +40,34 @@ private Optional<Personaje> findById(@PathVariable("id")Long idPersonaje){
 @GetMapping(params="name")
 private List<Personaje> findByName(@RequestParam("nombre")String nombre){
 	return personajeService.findByName(nombre);
+}
+@GetMapping(params="age")
+private List<Personaje> findByAge(@RequestParam("edad")Integer edad){
+	return personajeService.findByAge(edad);
+}
+@DeleteMapping(path="delete/{id}")
+private String delete(@PathVariable("id")Long idPersonaje) {
+	try {
+	personajeService.delete(idPersonaje);
+	return "Character deleted succesfully!";
+	} catch (Exception e) {
+		return "Couldn't delete character!";
+	} 
+}
+@PostMapping
+private Personaje save(@RequestParam("file")MultipartFile imagen,@ModelAttribute Personaje personaje) {
+	if(!imagen.isEmpty()) {
+		Path imagesPath=Paths.get("src/main/resources/static/imagenes");
+		String absolutPath=imagesPath.toFile().getAbsolutePath();
+		try {
+			byte[] bytes=imagen.getBytes();
+			Path route=Paths.get(absolutPath + imagen.getOriginalFilename());
+			Files.write(route, bytes);
+			personaje.setImagen(imagen.getOriginalFilename());
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	return personajeService.save(personaje);
 }
 }
